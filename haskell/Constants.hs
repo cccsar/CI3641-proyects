c2830b1305fe4b9149ff1faf946803a524dc0405
Luis Diaz <ldiazn98@gmail.com>
2021-02-13 15:49:44 -0400

    solo nos dejan entregar dos archivos :(

--- haskell/Constants.hs
+++
@@ -1,9 +0,0 @@
-module Constants where
-
--- String de presentacion
-introduction :: String
-introduction = "This is Haskinator! the great oracle!"
-
--- Nombres de opciones
-nombres :: [String]
-nombres = ["crear","predecir","persistir","cargar","consultar"]
