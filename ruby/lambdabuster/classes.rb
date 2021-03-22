408ea399cc906f33772bbbe85b92d877f88acf5a
Luis Diaz <ldiazn98@gmail.com>
2021-03-22 11:12:11 -0400

    fuck ruby

--- ruby/lambdabuster/classes.rb
+++ ruby/lambdabuster/classes.rb
@@ -1,27 +1,35 @@
 class SearchList
+
+    # Cannot modify, just get
+    attr_reader :list
+
+    # init with specified arguments as only members
     def initialize(*elems)
         @list = []
         for elem in elems
-            @list.append(elem)
-        end
-        for i in @list
-            puts @list[0]
+            @list << elem
         end
     end
 
+    # Add one element
     def <<(elem)
+        raise NotImplementedError
         @list<< elem
         self
     end
 
+    # return a string representation 
     def to_s
-    
+        return @list.join(", ")
     end
+
+    # Add this search list to other one
     def +(other )
-    
+        SearchList.new(@list + other.list)
     end
 
     def each (* args , &block )
+        raise NotImplementedError
         @list.each(* args , &block )
     end
 end
---
+++ ruby/lambdabuster/test.rb
@@ -0,0 +1,9 @@
+require './classes.rb'
+
+x = SearchList.new(1,2,3,4,5)
+y = SearchList.new(6,7,8)
+
+puts x
+puts y
+
+puts x + y
