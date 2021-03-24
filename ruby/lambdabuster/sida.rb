ffa5061054a11e17f81ca401e16492411e26142f
cccsar <15-11295@usb.ve>
2021-03-23 23:52:26 -0400

    anadi esquema sidoso para extraer atributos del json

--- ruby/lambdabuster/sida.rb
+++ ruby/lambdabuster/sida.rb
@@ -9,3 +9,36 @@ def getParsedJson filename
   end
 
 end
+
+
+# asume json bien formateado
+def actorsDirectorsTraversal parsedJson 
+
+  topLevel = ["directors","actors"]
+  attributes = ["name","birthday","nationality"] 
+
+  for kind in topLevel
+   for subjects in parsedJson[kind] 
+       for at in attributes
+         print "#{subjects[at]} " 
+       end
+       print "\n" 
+   end
+  end
+
+end
+
+
+# asume json bien formateado
+def moviesTraversal parsedJson
+
+  atts = ["name","runtime","categories","release-date","actores","price","rent-price","premiere","discount"]
+
+  for movie in parsedJson["movies"] 
+    for att in atts
+        print "#{movie[att]} " 
+      print "\n" 
+    end
+  end
+
+end
